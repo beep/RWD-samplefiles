abd3d0491aaeb67871059e113b327b06ce20079b
Ethan Marcotte <me@ethanmarcotte.com>
2014-04-27 16:21:20 -0400

    comments in core.js
    Signed-off-by: Ethan Marcotte <me@ethanmarcotte.com>

--- -/js/core.js
+++ -/js/core.js
@@ -1,11 +1,12 @@
 (function( $ ) {
-	// DOM-ready auto-init
 	$( function(){
 
-		// Slides!
+		// Let’s make a carousel!
 		// First, we’ll check to see if the viewport is wider than 500px.
 		if ( document.documentElement.clientWidth >= 500 ) {
 
+			// Now, let’s use $.get() to remotely fetch our slides.
+			// (Normally, I’d use a `data-` attribute on the `.slides` element to reference this HTML stub, but this simplifies the example a bit.)
 			$.get( "-/ajax/slides.html", function( data ) {
 				$( ".welcome .slides" )
 					.append( data )
